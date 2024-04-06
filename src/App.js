3af0b4ce3c3d8a2d986ac5a769b0f1bf07037885
Soulman2131 <sdrame81@gmail.com>
2024-04-06 13:28:00 +0200

    React vs Js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState();
+  const [count, setCount] = useState(0);
+
+  // FETCH ADVICE
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-light text-center m-3">
+      <h1 className="p-2 text-primary ">
+        {count < 1 ? "An advice" : <>{advice}</>}
+      </h1>
+      <Button variant="outline-success px-4 py-2 m-4" onClick={fetchAdvice}>
+        Click here to read
+      </Button>
+      <p className="bg-warning w-50 mx-auto text-black font-bold p-3">
+        You clicked <strong> {count} </strong> {count > 1 ? "times" : "time"}
+      </p>
     </div>
   );
-}
+};
 
 export default App;
